f75c05eba1d9cd7508bcbd4615c40cf488e2987e
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2024-10-04 19:25:17 +0600

    APP-12: initialized app with fcm + nativewind + paper (#1)
    * initialized app with fcm + nativewind + paper
    
    * changed theme to nike

---
+++ app/app/(tabs)/explore.tsx
@@ -0,0 +1,8 @@
+import { View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+    </View>
+  );
+}
---
+++ app/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { Image, StyleSheet, Platform, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default function HomeScreen() {
+  return (
+    <View>
+    </View>
+  );
+}
