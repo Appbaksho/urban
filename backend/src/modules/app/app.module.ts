5aeb94479b55574136b9af8bfa0bf624d4ff0c3c
Mdkaif2782 <mibnzaman02@gmail.com>
2024-10-06 01:32:08 +0600

    configured prisma + mongodb

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from '@nestjs/config'
-import { MongooseModule } from '@nestjs/mongoose';
-import { MulterModule } from '@nestjs/platform-express';
+
 import {FirebaseModule} from "../firebase/firebase.module";
+import { CustomerModule } from '../../customer/customer.module';
 @Module({
   imports: [
     //ConfigModule.forRoot(),MongooseModule.forRoot(process.env.MONGO_URL),MulterModule.register({ dest: './uploads', }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true
+    }),
+    CustomerModule,
     FirebaseModule
   ],
   controllers: [AppController],
