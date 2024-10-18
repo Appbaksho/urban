12118ea5ab8700dccb5e1cc7040b1f1a54897e31
Mdkaif2782 <mibnzaman02@gmail.com>
2024-10-19 01:49:58 +0600

    shop ui completed

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,81 +1,42 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
-import {ScrollView, View } from 'react-native';
-import { Text } from 'react-native-paper';
-import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
-import useLogout from '@/hooks/useLogout';
-import { useTestAuthMutation } from '@/modules/notification/api/notification.api';
-import { useNavigation } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import TopBar from '@/components/drawer/top-bar';
-import Chip from '@/components/home/chip';
-import ProductSectionHorizontal from '@/components/home/product-section';
-import { theme } from '@/theme/theme';
-import ProductSectionHolder from '@/components/home/product-section-holder';
-import ProductGrid from '@/components/home/product-grid';
+
+import TopBar from "@/components/drawer/top-bar";
+import CustomizeOption from "@/components/home/customize-opt";
+import FeaturedCard from "@/components/home/featured-card";
+import ProductSectionHorizontal from "@/components/home/product-section";
+import { useNavigation } from "expo-router";
+import { useLayoutEffect } from "react";
+import { View } from "react-native";
+import { ScrollView } from "react-native";
+import { Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  const [user, setUser] = useState<any>();
-  const [selectedChip, setSelectedChip] = useState<string>('Men');
   const navigation = useNavigation();
-  const logout = useLogout();
-  const auth = getAuth();
-  const [testAuth, { data, error }] = useTestAuthMutation();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, [navigation]);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
-      if (currentUser) {
-        setUser(currentUser);
-      } else {
-        setUser(undefined);
-      }
-    });
-
-    return () => unsubscribe();
-  }, [auth]);
-
-  useEffect(() => {
-    console.log(data, error);
-  }, [data, error]);
-
-    useEffect(() => {
-        console.log('selectedChip: ', selectedChip);
-    }, [selectedChip]);
-
+    useLayoutEffect(() => {
+        navigation.setOptions({
+          headerShown: false,
+        });
+      }, [navigation]);
   return (
-    <SafeAreaView className={'h-full bg-white items-center'}>
-      <TopBar name='Home' />
-      <View className={'flex-1'}>
-        <View className={'flex-row pt-3 pb-5 mx-4'}>
-          {['Men', 'Women', 'Anime'].map((label) => (
-            <Chip
-              key={label}
-              label={label}
-              selected={selectedChip === label}
-              onPress={() => setSelectedChip(label)}
-            />
-          ))}
-        </View>
-        <View className={'w-[100vw] bg-gray-300'} style={{height:1}}/>
-        <ScrollView showsVerticalScrollIndicator={false} className={'flex-1 w-full'}>
-            <ProductSectionHorizontal 
-              title={'Top picks for You'} 
-              subtitle={'Recommended products'} 
-              products={['as','as','asas','asas']} 
-              preview={true}
-              />
-            <ProductSectionHolder />
-            <ProductGrid  title={'Upcoming'} />
-            <ProductSectionHorizontal title={'Shop by category'} isCategoryList={true} preview={true}/>
-            <ProductGrid  title={'New Arrivals'} />
-            <View className={'w-full'} style={{height:30}}/>  
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+    <SafeAreaView className={'bg-white w-full h-full'}>
+    <TopBar name='Home' />
+    <ScrollView className={'h-full w-full'} showsVerticalScrollIndicator={false}>
+      <Text className={'my-2 ml-4'} variant={'titleLarge'}>Good Morning, Kaif!</Text>
+      <ProductSectionHorizontal 
+      title={'Top picks for You'} 
+      subtitle={'Recommended products'} 
+      products={['as','as','asas','asas']} 
+      preview={true}
+      />
+      <FeaturedCard title={"FLAT DISCOUNT 30%"} subtitle={"Anime ft. Tshirts"} image={"https://down-sg.img.susercontent.com/file/sg-11134201-7rd5r-lw49z0rw16nma5"}/>
+      <ProductSectionHorizontal 
+      title={'You May Like'} 
+      subtitle={'Lifestyle'} 
+      products={['as','as','asas','asas']} 
+      preview={true}
+      />
+      <CustomizeOption/>
+    </ScrollView>
+</SafeAreaView>
   );
 }
--- app/app/(tabs)/shop.tsx
+++ app/app/(tabs)/shop.tsx
@@ -1,23 +1,77 @@
-import TopBar from "@/components/drawer/top-bar";
-import { useNavigation } from "expo-router";
-import { useLayoutEffect } from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import {ScrollView, View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
+import useLogout from '@/hooks/useLogout';
+import { useTestAuthMutation } from '@/modules/notification/api/notification.api';
+import { useNavigation } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import TopBar from '@/components/drawer/top-bar';
+import Chip from '@/components/home/chip';
+import ProductSectionHorizontal from '@/components/home/product-section';
+import { theme } from '@/theme/theme';
+import ProductSectionHolder from '@/components/home/product-section-holder';
+import ProductGrid from '@/components/home/product-grid';
+
 
 const ShopLayout = () => {
-    const navigation = useNavigation();
-    useLayoutEffect(() => {
+      const [user, setUser] = useState<any>();
+      const [selectedChip, setSelectedChip] = useState<string>('Men');
+      const navigation = useNavigation();
+      const logout = useLogout();
+      const auth = getAuth();
+      const [testAuth, { data, error }] = useTestAuthMutation();
+    
+      useLayoutEffect(() => {
         navigation.setOptions({
           headerShown: false,
         });
       }, [navigation]);
+    
+      useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
+          if (currentUser) {
+            setUser(currentUser);
+          } else {
+            setUser(undefined);
+          }
+        });
+    
+        return () => unsubscribe();
+      }, [auth]);
+    
+      useEffect(() => {
+        console.log(data, error);
+      }, [data, error]);
+    
+        useEffect(() => {
+            console.log('selectedChip: ', selectedChip);
+        }, [selectedChip]);  
       
     return (
-        <SafeAreaView>
-            <TopBar name='Shop' />
-            <Text variant={'titleMedium'}>Shop Layout</Text>
-        </SafeAreaView>
+      <SafeAreaView className={'h-full bg-white items-center'}>
+      <TopBar name='Shop' />
+      <View className={'flex-1'}>
+        <View className={'flex-row pt-3 pb-5 mx-4'}>
+          {['Men', 'Women', 'Anime'].map((label) => (
+            <Chip
+              key={label}
+              label={label}
+              selected={selectedChip === label}
+              onPress={() => setSelectedChip(label)}
+            />
+          ))}
+        </View>
+        <View className={'w-[100vw] bg-gray-300'} style={{height:1}}/>
+        <ScrollView showsVerticalScrollIndicator={false} className={'flex-1 w-full'}>
+            <ProductSectionHorizontal title={'Shop by category'} isCategoryList={true} preview={true}/>
+            <ProductSectionHolder />
+            <ProductGrid  title={'Upcoming'} />
+            <ProductGrid  title={'New Arrivals'} />
+            <View className={'w-full'} style={{height:30}}/>  
+        </ScrollView>
+      </View>
+    </SafeAreaView>
     );
 }
 
