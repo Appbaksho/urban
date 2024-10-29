8409499b9b88e557b27f5b19c9ed1e9dab2e01a0
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-10-30 01:50:10 +0600

    created explore more section

--- urban-website/app/page.tsx
+++ urban-website/app/page.tsx
@@ -5,6 +5,8 @@ import Footer from "@/components/common/footer";
 import HeroSection from "@/components/home/hero-section";
 import LatestAndGreatest from "@/components/home/latest-and-greatest";
 import ShopOurIcons from "@/components/home/shop-our-icons";
+import NewInThisWeek from "@/components/home/new-in-this-week";
+import ExploreMore from "@/components/home/explore-more";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
     <HeroSection/>
     <LatestAndGreatest/>
     <ShopOurIcons/>
+    <NewInThisWeek/>
+    <ExploreMore/>
     <Footer/>
     </>
   );
