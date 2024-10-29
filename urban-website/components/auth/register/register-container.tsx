a5ea130cfe74e0039e2cfc6b9f1a154818aff964
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-10-29 23:13:22 +0600

    created auth page UI(s)

--- urban-website/components/auth/register/register-container.tsx
+++ urban-website/components/auth/register/register-container.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 import RegisterForm from './register-form'
 const RegisterContainer = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2'>
-      <Image src={image} placeholder='blur' height={800} width={700} alt='Urban' className='min-h-[80vh] object-cover md:block hidden'/>
-      <div className="flex items-center justify-center py-10 md:py-0">
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <Image src={image} placeholder='blur' height={800} width={700} alt='Urban' className='min-h-[60vh] object-cover md:block hidden'/>
+      <div className="flex justify-center py-10 md:py-0 md:col-span-2 p-3">
         <RegisterForm/>
       </div>
     </div>
--- urban-website/components/auth/register/validator/register.validator.ts
+++ urban-website/components/auth/register/validator/register.validator.ts
@@ -1,10 +1,11 @@
 import { z } from 'zod';
 
-const registerSchema = z.object({
+export const registerSchema = z.object({
     email: z.string().email({ message: "Invalid email address" }),
     password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
     confirmPassword: z.string().min(8, { message: "Confirm Password must be at least 8 characters long" }),
-    name: z.string().min(1, { message: "Name is required" }),
+    firstName: z.string().min(1, { message: "Name is required" }),
+    lastName: z.string().min(1, { message: "Name is required" }),
     billingAddress: z.string().min(1, { message: "Billing address is required" }),
     phoneNumber: z.string().min(10, { message: "Phone number must be at least 10 digits long" }),
     city: z.string().min(1, { message: "City is required" }),
@@ -14,4 +15,4 @@ const registerSchema = z.object({
     path: ["confirmPassword"]
 });
 
-export default registerSchema;
+export type RegistrationSchemaType = z.infer<typeof registerSchema>;
