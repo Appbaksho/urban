b95587938804b0e64061e01dc6ecd4426b060b57
Mdkaif2782 <mibnzaman02@gmail.com>
2024-12-13 17:48:34 +0600

    some fixes

--- backend/src/modules/customer/customer.controller.ts
+++ backend/src/modules/customer/customer.controller.ts
@@ -1,16 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
-import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { AccessTokenGuard } from 'src/middlewares/access-token.guard';
 import { User } from 'src/decorators/user.decorator';
 
@@ -38,16 +28,16 @@ export class CustomerController {
     return this.customerService.findOne(id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateCustomerDto: UpdateCustomerDto,
-  ) {
-    return this.customerService.update(+id, updateCustomerDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateCustomerDto: UpdateCustomerDto,
+  // ) {
+  //   return this.customerService.update(+id, updateCustomerDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customerService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.customerService.remove(+id);
+  // }
 }
---
+++ urban-website/app/product/[productId]/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Footer from '@/components/common/footer';
+import Navbar from '@/components/common/navbar';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+
+import { useParams } from 'next/navigation';
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+
+const ProductPage = () => {
+    const { productId } = useParams();
+    const [variant, setVariant] = React.useState(0);
+    const vatiantNames = ['Black', 'Green'];
+    const imageUrls1 = [
+        "https://d3j1z37yk0dbyk.cloudfront.net/media/images/238806_4_5__20231207090650959_width_1024.jpg",
+        "https://d3j1z37yk0dbyk.cloudfront.net/media/images/238806cu1__4_5__20231207090651751_width_1024.jpg",
+        "https://d3j1z37yk0dbyk.cloudfront.net/media/images/238806cu2__4_5__20231207090652708_width_1024.jpg"
+    ];
+
+    const imageUrls2 = [
+        "https://d3j1z37yk0dbyk.cloudfront.net/media/images/238807_4_5__20231207090653704_width_1024.jpg",
+        "https://d3j1z37yk0dbyk.cloudfront.net/media/images/238807cu1__4_5__20231207090654425_width_1024.jpg"
+    ];
+    return (
+    <>
+        <Navbar/>
+        <div className={'ms-[10vw] mt-4 h-fit'}>
+                <span className="text-sm text-gray-500">Home</span> | 
+                <span className="text-sm text-gray-500"> Man</span> | 
+                <span className="text-sm text-gray-500"> Sherwani</span> | 
+                <span className="text-sm text-gray-500"> Platinum </span> | 
+                <span className="text-sm font-semibold text-gray-700"> {productId}</span>
+            </div>
+        <div className={'mt-6 flex flex-row h-[80vh] mx-[10vw] gap-12'}>
+            <div className={'h-full flex-1'}>
+                <div className="relative h-full w-fit">
+                    <ChevronLeft size={40} className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-25 p-2  cursor-pointer" />
+                    <img src={imageUrls1[0]} className="h-full w-fit object-contain" />
+                    <ChevronRight size={40} className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-25 p-2  cursor-pointer" />
+                </div>
+            </div>
+            <div className={'h-full flex-1 flex'}>
+                <div className={""}>
+                    <div className="text-sm text-gray-950 font-semibold">Platinum Panjabi</div> 
+                    <div className="text-sm text-gray-500">{productId}</div> 
+                    <div className="text-sm mt-3 text-gray-950 font-bold"> BDT 17,750 </div>
+                </div>
+            </div>
+        </div>
+        <Footer/>
+    </>
+    );
+};
+
+
+export default ProductPage;
