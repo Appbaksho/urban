c97172429c529dc1b333301ec56ebcb5987600c0
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-12-29 09:51:44 +0600

    feat: implement authentication redirection and update navbar based on login state

--- urban-website/components/common/nav-top.tsx
+++ urban-website/components/common/nav-top.tsx
@@ -1,11 +1,14 @@
 
 import Link from 'next/link'
 import React from 'react'
-const NavbarTop = () => {
+interface NavbarTopProps {
+    isLoggedIn?: boolean
+}
+const NavbarTop = (props:NavbarTopProps) => {
   return (
     <div className='hidden bg-secondary md:px-20 px-5 py-2 md:flex items-center justify-between text-xs'>
         <p></p>
-        <ul className='flex items-center gap-2 [&>li>a]:text-xs [&>li>a]:font-semibold [&>li>a]:transition-all [&>li>a]:ease-in-out [&>li>a]:duration-150 hover:[&>li>a]:opacity-60'>
+        {!props.isLoggedIn&&<ul className='flex items-center gap-2 [&>li>a]:text-xs [&>li>a]:font-semibold [&>li>a]:transition-all [&>li>a]:ease-in-out [&>li>a]:duration-150 hover:[&>li>a]:opacity-60'>
             
             <li className='pl-2'>
                 <Link href="/auth/login">
@@ -18,7 +21,7 @@ const NavbarTop = () => {
                     Register
                 </Link>
             </li>
-        </ul>
+        </ul>}
     </div>
   )
 }
