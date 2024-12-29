93dcc4d915958c78c2a404bf281df542f6ab768d
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2024-12-30 02:01:13 +0600

    refactor dashboard layout and sidebar components for improved structure and UI

--- admin/components/app-sidebar.tsx
+++ admin/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { GalleryVerticalEnd, Minus, Plus } from "lucide-react"
+import { GalleryVerticalEnd, Minus, Plus, ShoppingBag } from "lucide-react"
 
 import { SearchForm } from "@/components/search-form"
 import {
@@ -171,17 +171,17 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
+                  <ShoppingBag className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
+                  <span className="font-semibold">Urban</span>
+                  <span className="">Admin</span>
                 </div>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
-        <SearchForm />
+        {/* <SearchForm /> */}
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
