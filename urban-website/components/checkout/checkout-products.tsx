1bfeee806f969aabbab5fe7b1f9d3048c79eb81e
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-01-01 21:12:22 +0600

    add offline checkout functionality: implement CheckoutProductOffline component and enhance total calculation for offline products

--- urban-website/components/checkout/checkout-products.tsx
+++ urban-website/components/checkout/checkout-products.tsx
@@ -7,11 +7,15 @@ import { useToast } from '@/hooks/use-toast'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '@/firebase/firebase'
 import { Skeleton } from '../ui/skeleton'
+import CheckoutProductOffline from './checkout-product-offline'
+import { AddToCartPayload } from '@/api/cart/cart.model'
 
 const CheckoutProducts = () => {
-  const [getCart,{data:cart,isError,isLoading,error}] = useLazyGetCartQuery()
+  const [getCart,{data:cart,isError,isSuccess,isLoading,error}] = useLazyGetCartQuery()
   const [isOffline, setisOffline] = useState<boolean>(false)
+  const [offlineProducts, setofflineProducts] = useState<AddToCartPayload[]>([])
   const {toast} = useToast()
+  const [total, settotal] = useState(0)
 
   useEffect(() => {
     if(isError){
@@ -22,7 +26,10 @@ const CheckoutProducts = () => {
       })
       console.log(error)
     }
-  }, [isError])
+    if(isSuccess){
+      settotal(cart?.items.reduce((acc,v)=>acc+v.size.product.price*v.quantity,0))
+    }
+  }, [isError,isSuccess])
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -39,6 +46,15 @@ const CheckoutProducts = () => {
       }
     })
   }, [])
+
+  useEffect(() => {
+    if(isOffline){
+      // settotal(JSON.parse(localStorage.getItem('cart')||'[]').reduce((acc:any,v:any)=>acc+v.size.product.price*v.quantity,0))
+      setofflineProducts(JSON.parse(localStorage.getItem('cart')||'[]'))
+
+    }
+  }, [isOffline])
+  
   
 
   return (
@@ -48,8 +64,9 @@ const CheckoutProducts = () => {
         <CardContent className='py-5'>
           <CardDescription className='text-xs'>Scroll Down if you cannot see all products</CardDescription>
           <ScrollArea className="h-[200px] w-full p-1">
-            {
-              isLoading?Array(5).fill(0).map((v,i)=>{
+            {isOffline?offlineProducts.map((v)=>{
+              return <CheckoutProductOffline products={offlineProducts} settotal={settotal} quantity={Number(v.quantity)} id={v.productId} key={v.sizeId} />
+            }):isLoading?Array(5).fill(0).map((v,i)=>{
                 return <Skeleton className="w-full h-[80px]"/>
               }):cart?.items.map((v)=>{
                 return <CheckoutProduct key={v.id} {...v}/>
@@ -58,7 +75,7 @@ const CheckoutProducts = () => {
           </ScrollArea>
         </CardContent>
         <CardFooter>
-          <p className='text-right flex-1 w-full'>Total : <span className="font-bold">{cart?.items.reduce((acc,v)=>acc+v.size.product.price*v.quantity,0)} BDT</span></p>
+          <p className='text-right flex-1 w-full'>Total : <span className="font-bold">{total} BDT</span></p>
         </CardFooter>
       </Card>
     </div>
