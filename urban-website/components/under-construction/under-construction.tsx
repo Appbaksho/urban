14f3acf52752b2326ac65035e993a1b1576b4ea0
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-01-15 14:41:04 +0600

    feat: add UnderConstructionPage component with layout and styling

--- urban-website/components/under-construction/under-construction.tsx
+++ urban-website/components/under-construction/under-construction.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import cons from '@/public/cons.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { buttonVariants } from '../ui/button'
+import { Facebook, FacebookIcon } from 'lucide-react'
 const UnderConstructionPage = () => {
   return (
-    <div className="fixed">UnderConstructionPage</div>
+    <div className="fixed flex-col z-50 flex items-center justify-center bg-white top-0 left-0 right-0 bottom-0 h-screen w-screen">
+      <Image src={cons} height={300} width={300} className='w-[300px] h-auto' alt=''/>
+      <p className='text-center font-bold text-4xl mt-10'>Coming Soon...</p>
+      <Link href='https://www.facebook.com/profile.php?id=61571611291078&mibextid=ZbWKwL' className={buttonVariants({className:'mt-5'})}>
+        <FacebookIcon stroke='0' fill='white' size={16} className='mr-2' /> Facebook
+      </Link>
+    </div>
   )
 }
 
